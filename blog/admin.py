9e948707e910597297b3426dc9cba5312334a2ee
hasantopan <hasan_topan@hotmail.com>
2022-04-30 10:39:03 +0300

    yazi admin paneli düzenlendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from blog.models import KategoriModel
+from blog.models import KategoriModel, YazilarModel
 
 admin.site.register(KategoriModel)
+
+class YazilarAdmin(admin.ModelAdmin):
+    search_fields =('baslik','icerik')
+    list_display =(
+        'baslik','olusturulma_tarihi','duzenleme_tarihi'
+    )
+
+
+
+admin.site.register(YazilarModel, YazilarAdmin)
+
--- blog/models/yazi.py
+++ blog/models/yazi.py
@@ -1,23 +1,24 @@
-from enum import unique
-from os import popen
-from tabnanny import verbose
-from tkinter.tix import PopupMenu
 from django.db import models
 from autoslug import AutoSlugField
 from blog.models import KategoriModel
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
 
 class YazilarModel(models.Model):
     resim = models.ImageField(upload_to ='yazi_resimleri')
     baslik = models.CharField(max_length=50)
-    icerik = models.TextField()
+    icerik = RichTextField()
     olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
     duzenleme_tarihi = models.DateTimeField(auto_now=True)
     slug = AutoSlugField(populate_from = 'baslik', unique=True)
     kategoriler = models.ManyToManyField(KategoriModel, related_name='yazi')
-    yazar = models.ForeignKey(User, on_delate=models.CASCADE related_name='yazilar')
+    yazar = models.ForeignKey(User, on_delete=models.CASCADE, related_name='yazilar')
     
     class Meta:
         verbose_name ='Yazi'
         verbose_name_plural = 'Yazilar'
-        db_name = 'Yazi'
+        db_table = 'Yazi'
+        
+    def __str__(self):
+        return self.baslik
